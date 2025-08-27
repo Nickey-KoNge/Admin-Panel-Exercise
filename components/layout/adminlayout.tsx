a3aa09bb2ffd5878cacef3e48b6eed7d55f2f151
Nickey <nickey@192.168.68.117>
2025-08-27 14:56:25 +0630

    add motion effect on layout with page transition animation

--- components/layout/adminlayout.tsx
+++ components/layout/adminlayout.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import React from 'react';
 import Nav from '../navigation/nav';
 import TopNavBar from '../navigation/topnav';
 import styles from '../../styles/admin/layouts/adminlayout.module.scss';
-
+import { motion, AnimatePresence } from 'framer-motion';
+import { usePathname } from 'next/navigation';
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+    const pathname = usePathname();
+
     return (
         <div className={styles.layoutContainer}>
             <aside className={styles.sidenav}>
@@ -16,9 +21,21 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
                    <TopNavBar /> 
                 </header>
                 
-                <main className={styles.content}>
+                {/* <main className={styles.content}>
                     {children}
-                </main>
+                </main> */}
+                <AnimatePresence mode='wait'>
+                    <motion.main
+                    key={pathname}
+                    className={styles.content}
+                    initial = {{opacity: 0,y:20}} //start animation transaction
+                    animate= {{opacity: 1, y:0}}
+                    exit = {{opacity: 0 , y: -20}} // end animation stransaction
+                    transition = {{duration: 0.7}}
+                    >
+                        {children}
+                    </motion.main>
+                </AnimatePresence>
             </div>
         </div>
     );
